a9f7d572df566a0a99d78cd62fcae5317a02c12e
wahke <wahke@icloud.com>
2026-05-17 17:22:02 +0200

    Fix command evaluation syntax in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,4 +25,4 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 
 # Run the server
-eval ${PARSED}
+eval "$PARSED"
